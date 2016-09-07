7ab4aa6f92d4096a75ef5cd9d16fbd6a15269771
CETHER <robert.hct@gmail.com>
2016-09-07 14:42:34 -0500

    Commit 2
    Se agrega la carpeta my web pokedex

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert  git -y
 
-sudo git clone https://github.com/CETHER/OpenStack.git/my-web-pokedex/. /var/www/html/
+sudo git clone https://github.com/openstack-hackathon/training-shade-cucea.git
+sudo cp -r training-shade-cucea/challenge3/my-web-pokedex/. /var/www/html/

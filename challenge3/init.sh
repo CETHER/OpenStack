e0b9e184caa5c2ad64b707ab8f2f5f44a5d46411
CETHER <robert.hct@gmail.com>
2016-09-07 15:24:04 -0500

    Commit 4
    cambio en el archivo init.sh, copiar carpeta my'web'pokedex en servidor

--- challenge3/init.sh
+++ challenge3/init.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert  git -y
 
 sudo git clone https://github.com/CETHER/OpenStack.git
-sudo cp -r my-web-pokedex/. /var/www/html/
+sudo cp -r  OpenStack/challenge3/my-web-pokedex/. /var/www/html/

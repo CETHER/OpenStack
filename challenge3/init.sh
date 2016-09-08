687b2c7b9ecd22ab1ce8eccdc15aba221bfbda74
CETHER <robert.hct@gmail.com>
2016-09-07 19:30:26 -0500

    Commit 5
    Cambio en init

--- challenge3/init.sh
+++ challenge3/init.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert  git -y
 
-sudo git clone https://github.com/CETHER/OpenStack.git
-sudo cp -r  OpenStack/challenge3/my-web-pokedex/. /var/www/html/
+#sudo git clone https://github.com/CETHER/OpenStack.git
+#sudo cp -r  OpenStack/challenge3/my-web-pokedex/. /var/www/html/
